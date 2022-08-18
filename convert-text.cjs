abceff57dd8a76347b51bd61a2c33aba33c0688d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-08-19 00:19:26 +0100

    convert to cjs

--- convert-text.cjs
+++ convert-text.cjs
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-fs.readFile("./words.txt", "utf8", (err, data) => {
+fs.readFile("./words_alpha.txt", "utf8", (err, data) => {
   if (err) throw err;
 
   let words = data.split("\n");
