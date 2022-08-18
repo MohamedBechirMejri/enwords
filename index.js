ac3ae05c6718f5fd77d9dc356d7e75fe5adddf78
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-08-19 00:03:56 +0100

    switch to ts

--- index.js
+++
@@ -1,7 +0,0 @@
-import words from "./words.json";
-
-import dictionaryWords from "./dictionary-words.json";
-
-export dictionaryWords
-
-export default words;
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import words from "./words.json";
+
+export default words;
