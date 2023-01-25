80dafaa94361141988a4b281fb00d1cb02848fb4
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-25 09:31:55 +0100

    use import instead of require

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import json from "@rollup/plugin-json";
 
-const packageJson = require("./package.json");
+import packageJson from "./package.json" assert { type: "json" };
 
 export default {
   input: "src/index.ts",
